0b88e4df75c6796918fb1821642c81872aea014b
AwertoQ <sasha.razgudaev@gmail.com>
2024-07-22 21:32:30 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+number = 0
+i = 0
+while i != len(my_list):
+    if my_list[i] >= number:
+        print(my_list[i])
+        i += 1
+    elif my_list[i] < number:
+        del my_list[i]
+        continue
+    else:
+        break
