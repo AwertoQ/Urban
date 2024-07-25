85882bf3f6a01b1f3614516f9d06186825e51e77
AwertoQ <sasha.razgudaev@gmail.com>
2024-07-25 21:04:05 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,20 @@
+import  random
+def random_namber():
+    fiest_namber = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    key_1 = random.choice(fiest_namber)
+    return key_1
+
+key_1 = random_namber()
+
+def pasword():
+    key_2 = ''
+    a = 0
+    for i in range(1, key_1):
+        for j in range(i + 1, key_1):
+            if key_1 == i + j:
+                key_2 += str(i) + str(j)
+                a +=1
+    return key_2
+
+key_2 = pasword()
+print(key_1, '-', key_2)
